cc4c5d4a6e2f64280c261e6ea25566414987f34e
aydisofyan <aydisofyan6@gmail.com>
2025-06-23 00:49:19 +0700

    tamabah splash screens dan login

---
+++ lib/navigation.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+import 'package:uas_mobile/musikPlayer.dart';
+import 'homepage.dart';
+
+class NavigationPage extends StatefulWidget {
+  const NavigationPage({Key? key}) : super(key: key);
+
+  @override
+  State<NavigationPage> createState() => _NavigationPageState();
+}
+
+class _NavigationPageState extends State<NavigationPage> {
+  int _selectedIndex = 0;
+
+  final List<Widget> _children = [
+    Homepage(), // Home// Placeholder
+    MusicPlayer(currentSong),  // Placeholder
+    Center(child: Text("Profile Page", style: TextStyle(color: Colors.white))),  // Placeholder
+  ];
+
+  void _onTap(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: _children[_selectedIndex],
+      bottomNavigationBar: BottomNavigationBar(
+        backgroundColor: Colors.black,
+        selectedItemColor: Colors.white,
+        unselectedItemColor: Colors.grey,
+        currentIndex: _selectedIndex,
+        onTap: _onTap,
+        items: const [
+          BottomNavigationBarItem(icon: Icon(Icons.home), label: 'Home'),
+          BottomNavigationBarItem(icon: Icon(Icons.play_circle_fill), label: 'Play', ),
+          BottomNavigationBarItem(icon: Icon(Icons.person), label: 'Profile'),
+        ],
+      ),
+    );
+  }
+}
